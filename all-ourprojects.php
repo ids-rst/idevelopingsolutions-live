e78ce4d535024600d01b5ceb43b7f77730bf9e50
ids-shubham <shubhammehra@idevelopingsolutions.com>
2024-07-05 11:03:22 +0530

    i have removed extra commented code and aligned all css

--- all-ourprojects.php
+++ all-ourprojects.php
@@ -1,120 +1,129 @@
-<?php include 'header.php';?>   
+<?php include 'header.php';?>
 <!-- Breadcrumbs Start -->
 <div class="rs-breadcrumbs img3">
-    <div class="breadcrumbs-inner text-center">
-        <h1 class="page-title">Our Projects</h1>
-        <ul>
-            <li title="Home">
-                <a class="active" href="index.html">Home</a>
-            </li>
-            <li>Our Projects</li>
-        </ul>
-    </div>
+  <div class="breadcrumbs-inner text-center">
+    <h1 class="page-title">Our Projects</h1>
+    <ul>
+      <li title="Home">
+        <a class="active" href="index.html">Home</a>
+      </li>
+      <li>Our Projects</li>
+    </ul>
+  </div>
 </div>
 <!-- Breadcrumbs End -->
 
 <!-- Project Section Start -->
 <div class="rs-project style5 pt-120 pb-120 md-pt-80 md-pb-80">
-    <div class="container">
-        <div class="sec-title2 text-center mb-45">
-            <h2 class="title">Projects that We have Done</h2>
-            <div class="heading-line"></div>
-            <div class="desc desc-text">
-            We are a team of young, passionate, enthusiastic, and creative individuals with the capabilities to deliver state-of-the-art software products.
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-lg-4 col-md-6 mb-30">
-                <div class="project-item">
-                    <div class="project-img">
-                        <img src="assets/images/project/main-home/1.jpg" alt="images">
-                    </div>
-                    <div class="project-content">                                
-                        <div class="p-icon"><a href="case-studies-single.html"><i class="fa fa-angle-right"></i></a></div>
-                        <div class="project-inner">
-                            <h3 class="title"><a href="case-studies-single.html">IT Technology</a></h3>
-                            <span class="category"><a href="case-studies-single.html">Product Engineering</a></span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-6 mb-30">
-                <div class="project-item">
-                    <div class="project-img">
-                        <img src="assets/images/project/main-home/2.jpg" alt="images">
-                    </div>
-                    <div class="project-content">                                
-                        <div class="p-icon"><a href="case-studies-single.html"><i class="fa fa-angle-right"></i></a></div>
-                        <div class="project-inner">
-                            <h3 class="title"><a href="case-studies-single.html">IT Technology</a></h3>
-                            <span class="category"><a href="case-studies-single.html">Analytic Solutions</a></span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-6 mb-30">
-                <div class="project-item">
-                    <div class="project-img">
-                        <img src="assets/images/project/main-home/3.jpg" alt="images">
-                    </div>
-                    <div class="project-content">                                
-                        <div class="p-icon"><a href="case-studies-single.html"><i class="fa fa-angle-right"></i></a></div>
-                        <div class="project-inner">
-                            <h3 class="title"><a href="case-studies-single.html">IT Technology</a></h3>
-                            <span class="category"><a href="case-studies-single.html">Project Design</a></span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-6 sm-mb-25">
-                <div class="project-item">
-                    <div class="project-img">
-                        <img src="assets/images/project/main-home/4.jpg" alt="images">
-                    </div>
-                    <div class="project-content">                                
-                        <div class="p-icon"><a href="case-studies-single.html"><i class="fa fa-angle-right"></i></a></div>
-                        <div class="project-inner">
-                            <h3 class="title"><a href="case-studies-single.html">IT Technology</a></h3>
-                            <span class="category"><a href="case-studies-single.html">Growth Strategies</a></span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-6 sm-mb-25">
-                <div class="project-item">
-                    <div class="project-img">
-                        <img src="assets/images/project/main-home/5.jpg" alt="images">
-                    </div>
-                    <div class="project-content">                                
-                        <div class="p-icon"><a href="case-studies-single.html"><i class="fa fa-angle-right"></i></a></div>
-                        <div class="project-inner">
-                            <h3 class="title"><a href="case-studies-single.html">It Technology</a></h3>
-                            <span class="category"><a href="case-studies-single.html">Platform Integration</a></span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-6">
-                <div class="project-item">
-                    <div class="project-img">
-                        <img src="assets/images/project/main-home/6.jpg" alt="images">
-                    </div>
-                    <div class="project-content">                                
-                        <div class="p-icon"><a href="case-studies-single.html"><i class="fa fa-angle-right"></i></a></div>
-                        <div class="project-inner">
-                            <h3 class="title"><a href="case-studies-single.html">It Technology</a></h3>
-                            <span class="category"><a href="case-studies-single.html">Innovative Interfaces</a></span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+  <div class="container">
+    <div class="sec-title2 text-center mb-45">
+      <h2 class="title">Projects that We have Done</h2>
+      <div class="heading-line"></div>
+      <div class="desc desc-text">
+        We are a team of young, passionate, enthusiastic, and creative individuals with the capabilities to deliver state-of-the-art software products.
+      </div>
     </div>
+    <div class="row">
+      <div class="col-lg-4 col-md-6 mb-30">
+        <div class="project-item">
+          <div class="project-img">
+            <img src="assets/images/project/main-home/1.jpg" alt="images" />
+          </div>
+          <div class="project-content">
+            <div class="p-icon">
+              <a href="case-studies-single.html"><i class="fa fa-angle-right"></i></a>
+            </div>
+            <div class="project-inner">
+              <h3 class="title"><a href="case-studies-single.html">IT Technology</a></h3>
+              <span class="category"><a href="case-studies-single.html">Product Engineering</a></span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-6 mb-30">
+        <div class="project-item">
+          <div class="project-img">
+            <img src="assets/images/project/main-home/2.jpg" alt="images" />
+          </div>
+          <div class="project-content">
+            <div class="p-icon">
+              <a href="case-studies-single.html"><i class="fa fa-angle-right"></i></a>
+            </div>
+            <div class="project-inner">
+              <h3 class="title"><a href="case-studies-single.html">IT Technology</a></h3>
+              <span class="category"><a href="case-studies-single.html">Analytic Solutions</a></span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-6 mb-30">
+        <div class="project-item">
+          <div class="project-img">
+            <img src="assets/images/project/main-home/3.jpg" alt="images" />
+          </div>
+          <div class="project-content">
+            <div class="p-icon">
+              <a href="case-studies-single.html"><i class="fa fa-angle-right"></i></a>
+            </div>
+            <div class="project-inner">
+              <h3 class="title"><a href="case-studies-single.html">IT Technology</a></h3>
+              <span class="category"><a href="case-studies-single.html">Project Design</a></span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-6 sm-mb-25">
+        <div class="project-item">
+          <div class="project-img">
+            <img src="assets/images/project/main-home/4.jpg" alt="images" />
+          </div>
+          <div class="project-content">
+            <div class="p-icon">
+              <a href="case-studies-single.html"><i class="fa fa-angle-right"></i></a>
+            </div>
+            <div class="project-inner">
+              <h3 class="title"><a href="case-studies-single.html">IT Technology</a></h3>
+              <span class="category"><a href="case-studies-single.html">Growth Strategies</a></span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-6 sm-mb-25">
+        <div class="project-item">
+          <div class="project-img">
+            <img src="assets/images/project/main-home/5.jpg" alt="images" />
+          </div>
+          <div class="project-content">
+            <div class="p-icon">
+              <a href="case-studies-single.html"><i class="fa fa-angle-right"></i></a>
+            </div>
+            <div class="project-inner">
+              <h3 class="title"><a href="case-studies-single.html">It Technology</a></h3>
+              <span class="category"><a href="case-studies-single.html">Platform Integration</a></span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-6">
+        <div class="project-item">
+          <div class="project-img">
+            <img src="assets/images/project/main-home/6.jpg" alt="images" />
+          </div>
+          <div class="project-content">
+            <div class="p-icon">
+              <a href="case-studies-single.html"><i class="fa fa-angle-right"></i></a>
+            </div>
+            <div class="project-inner">
+              <h3 class="title"><a href="case-studies-single.html">It Technology</a></h3>
+              <span class="category"><a href="case-studies-single.html">Innovative Interfaces</a></span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
 <!-- Project Section End -->
 
-</div> 
 <!-- Main content End -->
 <?php include 'footer.php';?>
-     
-       
--- header.php
+++ header.php
@@ -70,11 +70,7 @@
                               <i class="flaticon-call"></i>
                               <a href="tel:+91-186-3590033"> +91-186-3590033</a>
                            </li>
-                           <!-- <li>
-                           <a href="https://www.google.com/maps/dir//Incredible+Developing+Solutions,+Dalhousie+Road,+near+Axis+Bank,+Pathankot,+Punjab+145001/@32.2024032,75.   4262128,11z/data=!4m8!4m7!1m0!1m5!1m1!1s0x391c80e488d9abc1:0xbac1c81dab431a0f!2m2!1d75.6558872!2d32.2700392" target="_blank">
-                              <i class="flaticon-location"></i> Dalhousie Road, Pathankot(145001)
-                           </a>
-                           </li> -->
+                           
                         </ul>
                      </div>
                      <div class="col-lg-4 text-right">
@@ -82,7 +78,6 @@
                            <ul>
                               <li class="opening"> <em><i class="flaticon-clock"></i> 09:00am-6:00pm</em> </li>
                               <li><a href="https://www.facebook.com/IDevelopingSolutions/" target="_blank"><i class="fa fa-facebook"></i></a></li>
-                              <!-- <li><a href="https://twitter.com/"><i class="fa fa-twitter"></i></a></li> -->
                               <li><a href="https://www.instagram.com/ideveloping2020/?igshid=ZDdkNTZiNTM%3D" target="_blank"><i class="fa fa-instagram"></i></a></li>
                            </ul>
                         </div>
@@ -139,11 +134,7 @@
                            <!-- //.main-menu -->
                            <div class="expand-btn-inner search-icon hidden-md">
                               <ul>
-                                 <!-- <li class="sidebarmenu-search">
-                                    <a class="hidden-xs rs-search" data-target=".search-modal" data-toggle="modal" href="#">
-                                    <i class="flaticon-search"></i>
-                                    </a>
-                                 </li> -->
+                              
                                  <li><a class="quote-btn" href="contact.php" onclick="showModel();">Get A Quote</a></li>
                               </ul>
                            </div>
@@ -158,14 +149,7 @@
       </div>
       <!--Full width header End-->
         <?php 
-            // $to = "contact@idevelopingsolutions.com";
-            // $subject = "";
-            // $txt = $_POST['name']."<br>". $_POST['phone']."<br>".$_POST['email']."<br>".$_POST['message'] ;
-            // $headers = "From: ".$_POST['email']."" . "\r\n";
-        
-            // if( isset($_POST['insert']) ){
-            // mail($to,$subject,$txt,$headers);
-            // }
+
         ?>
       
  
