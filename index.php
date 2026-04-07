195f0308751f07b3432b3f194c128a06fc924f19
Rajinder singh <vivek@idevelopingsolutions.com>
2026-04-07 11:09:05 +0530

    Correction

--- index.php
+++ index.php
@@ -756,7 +756,7 @@
                      </div>
                      <div class="project-content">
                         <div class="portfolio-inner">
-                           <h3 class="title"><a>latform Integration</a></h3>
+                           <h3 class="title"><a>Platform Integration</a></h3>
                            <span class="category"><a>IT Technology</a></span>
                         </div>
                      </div>
